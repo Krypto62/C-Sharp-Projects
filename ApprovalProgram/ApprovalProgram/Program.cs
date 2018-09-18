219e0226f5e503168b490c48a2ac88c134c4df82
Scott <scf2005@gmail.com>
2018-09-17 20:04:52 -0700

    Revamped with different approach

--- ApprovalProgram/ApprovalProgram/Program.cs
+++ ApprovalProgram/ApprovalProgram/Program.cs
@@ -8,31 +8,33 @@ namespace ApprovalProgram
         {
             Console.WriteLine("--- Car Insurance Questionaire ---");
             Console.WriteLine();
+
+            // Determine car insurance elegibility
+
+            // What is your age?
             Console.WriteLine("What is your age?");
-            string input1 = Console.ReadLine();
-            int num1 = Convert.ToByte(input1);
+            string ageInput = Console.ReadLine();
+            int userAge = Convert.ToUInt16(ageInput);
             Console.WriteLine();
 
-            string duiTrue = "yes";
-            string duiFalse = "no";
-            Console.WriteLine("Have you ever had a DUI?");
-            string input2 = Console.ReadLine();
-            //bool val1 = Convert.ToBoolean(input2);
+            // Have you ever had a DUI?
+            Console.WriteLine("Have you ever had a DUI? Please answer \"true\" or \"false\".");
+            string duiInput = Console.ReadLine();
+            bool userDUI = Convert.ToBoolean(duiInput);
             Console.WriteLine();
 
-            string speedTrue = "yes";
-            string speedFalse = "no";
-            Console.WriteLine("Do you have more than 3 speeding tickets?");
-            string input3 = Console.ReadLine();
-            Console.WriteLine();
+            // More than 3 speeding tickets?
+            Console.WriteLine("How many speeding tickets do you have?");
+            string ticketInput = Console.ReadLine();
+            int userTicket = Convert.ToUInt16(ticketInput);
 
-            Console.WriteLine("Qualified?");
-            bool isQualified = (num1 > 15 && duiFalse == input2 && speedFalse == input3);
-            Console.WriteLine(isQualified);
-            //bool isNotQualified = (num1 <= 15 && duiTrue == "yes" && speedFalse == "no");
-
-            
+            //Approved for insurance?
             Console.WriteLine();
+            Console.WriteLine("Check below to see if you have been approved for insurance.");
+            Console.WriteLine();
+            Console.WriteLine(userAge > 15 && userDUI == false && userTicket < 4);
+            Console.WriteLine();
+            Console.ReadLine();
 
         }
     }
