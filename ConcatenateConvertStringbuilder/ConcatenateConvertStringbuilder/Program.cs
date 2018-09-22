4bd3aa86fcefe59b402ee34f27a553c8741f276b
Scott <scf2005@gmail.com>
2018-09-21 19:42:17 -0700

    Added comments

--- ConcatenateConvertStringbuilder/ConcatenateConvertStringbuilder/Program.cs
+++ ConcatenateConvertStringbuilder/ConcatenateConvertStringbuilder/Program.cs
@@ -10,18 +10,22 @@ namespace ConcatenateConvertStringbuilder
     {
         static void Main(string[] args)
         {
-            
+            //Concatenate three strings.
             string how = "By working hard, ";
             string length = "in record time.";
             
             Console.WriteLine("What is your name?");
             string userName = (Console.ReadLine());
+
+            //Convert a string to uppercase.
             userName = userName.ToUpper();
             Console.WriteLine("");
             Console.WriteLine("");
             Console.WriteLine(how + "you will complete this course " + length + " We want to tell you " + userName + ", Good Job!");
             Console.WriteLine("");
             Console.WriteLine("");
+
+            //Create a Stringbuilder and build a paragraph of text, one sentence at a time.
             StringBuilder sb = new StringBuilder("The fruitage of the spirit ", 50);
             
             sb.Append("is love, ");
