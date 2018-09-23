5125d95e8e2c007501922ede326b5ac35658c365
Scott <scf2005@gmail.com>
2018-09-22 17:58:54 -0700

    Added numArray

---
+++ ArraysAndLists/ArraysAndLists/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArraysAndLists
+{
+    class Program
+    {
+        static void Main()
+        {
+            //1. Create a one-dimensional Array of strings. Ask the user to select an index of the Array and then display the string at that index on the screen.
+
+            Console.WriteLine();
+            string[] stringArray = new[] { "1 Dozen Eggs", "1 loaf of Bread", "1 8oz Yogurt", "1 Can of Soda", "1 8oz Chips", "1 Fresh Tomato" };
+            
+            Console.WriteLine("Please pick a number from 0 to 5 to see what grocery item you have won!");
+            string userInput1 = Console.ReadLine();
+            int userNum1 = Convert.ToByte(userInput1);
+
+            Console.WriteLine("You have won: " + (stringArray[userNum1]));
+            Console.WriteLine();
+
+            //2.Create a one-dimensional Array of integers.Ask the user to select an index of the Array and then display the integer at that index on the screen.
+            //3.Add in a message that displays when the user selects an index that doesn’t exist.
+            //4.Create a List of strings. Ask the user to select an index of the List and then display the content at that index on the screen.
+
+            int[] numArray = { 5, 10, 20, 30, 40 };
+
+            Console.WriteLine("About how much do you typically spend for snacks at the theater?");
+            Console.WriteLine("     Please select a number from 0 to 4...  \n \t 0 = Nothing \n \t 1 = $10 \n \t 2 = $20 \n \t 3 = $30 \n \t 4 = At least $40");
+            string userInput2 = Console.ReadLine();
+            int userNum2 = Convert.ToByte(userInput2);
+            Console.WriteLine("You selected #" + userNum2);
+            //Console.WriteLine("You selected # " + (numArray[userNum2]))
+            Console.WriteLine("Good news! We have decided to add " + (numArray[userNum2]) + " dollars to your snack budget:");
+           
+            Console.ReadLine();
+        }
+    }
+}
