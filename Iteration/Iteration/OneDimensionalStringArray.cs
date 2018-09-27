d84c4a0d59c41ad9058df14dff9d106fa1af0ba3
Scott <scf2005@gmail.com>
2018-09-26 20:32:56 -0700

    Created new branch just for String Array

---
+++ Iteration/Iteration/OneDimensionalStringArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //1.Create a one-dimensional Array of strings. Ask the user to input some text.
+        //Create a loop that goes through each string in the Array, adding the user’s text to the string.
+        //Then create a loop that prints off each string in the Array on a separate line.
+
+        string[] fruits = { "kiwi", "banana", "kumquat", "papaya" };
+        Console.WriteLine("What kind of tropical pet do you own?");
+        string userAnimal = Console.ReadLine();
+
+        for (int f = 0; f < fruits.Length; f++)
+
+        {
+            Console.WriteLine("Would your " + userAnimal + " like a " + fruits[f] + "?");
+        }
+
+        Console.ReadLine();
+            
+        
+
+    }
+}
---
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,5 @@
+date = "12/25/2025"
+full_date=date.split('/')
+print ('Month:' + full_date[0] + 'Day:' + full_date[1] + 'Year:' + full_date[2])
+
+#The above is from page 44 of "Learn Coding Basics in hours"
