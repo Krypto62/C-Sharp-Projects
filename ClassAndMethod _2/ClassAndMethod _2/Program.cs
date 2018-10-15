d34ab878bbe38e4c9a3148d52bbc7f173d00192b
Scott <scf2005@gmail.com>
2018-10-15 15:47:21 -0700

    Initial Commit

--- ClassAndMethod _2/ClassAndMethod _2/Program.cs
+++ ClassAndMethod _2/ClassAndMethod _2/Program.cs
@@ -8,15 +8,11 @@ namespace ClassAndMethod__2
 {
     class Program
     {
+        public static int num3 { get; private set; }
+
         static void Main(string[] args)
         //1. Create a class. In that class, create a void method that takes two integers as parameters(arguments).
         //Have the method do a math operation on the first integer and display the second integer to the screen.
-
-        //2. In the Main() method of the console app, instantiate the class.
-
-        //3. Call the method in the class, passing in two numbers.
-
-        //4. Call the method in the class, specifying the parameters by name.
         {
             Console.WriteLine("Please enter a number");
             int num1 = Convert.ToInt32(Console.ReadLine());
@@ -24,16 +20,34 @@ namespace ClassAndMethod__2
             Console.WriteLine("Thank you. Now please enter another number");
             int num2 = Convert.ToInt32(Console.ReadLine());
 
-            Add (500, num1, num2);
+        //3. Call the method in the class, passing in two numbers.
+            DoStuff.Add (500, num1, num2);
+            Console.WriteLine();
+            Console.WriteLine("---Press ENTER to learn how old my dog is---");
+            Console.ReadKey();
+
+        //2. In the Main() method of the console app, instantiate the class.
+        //4. Call the method in the class, specifying the parameters by name.
+            DoStuff dog = new DoStuff();
+            Console.WriteLine();
+            Console.WriteLine(dog.name + " is " + dog.age + " years old");
             Console.ReadKey();
         }
 
-        public static void Add (int num0, int num1, int num2)
+    }
+
+    public class DoStuff
+    {
+       
+        public static void Add(int num0, int num1, int num2)
         {
-            Console.WriteLine("Your first number + 500 = "+ (num0 + num1));
+            Console.WriteLine("Your first number + 500 = " + (num0 + num1));
             Console.WriteLine();
             Console.WriteLine("The second number you added is " + num2);
-        
         }
+
+        public string name = "Oliver";
+        public int age = 2;
+
     }
 }
