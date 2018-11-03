f3d0fb7347845ad6bae94fa102f81515cdddc776
Scott <scf2005@gmail.com>
2018-11-02 18:48:02 -0700

    Initial Commit

---
+++ ClassAndMethod_5/ClassAndMethod_5/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ClassAndMethod_5
+{
+    //1. Create a class. In that class, create a void method that outputs
+    //an integer. Have the method divide the data passed to it by 2.
+
+    public class Car
+    {
+        public void Model(int num1)
+        {
+            int num2 = num1 / 2;
+            Console.WriteLine("Your number divided by 2 is " + num2);
+            Console.WriteLine("Press \"Enter\" to continue...");
+            Console.ReadLine();
+        }
+        //4. Create a method with output parameters.
+        //5. Overload a method.
+        public double Model(double num3)
+        {
+            double num4 = num3 / 2;
+            return num4;
+        }
+
+    }
+
+    //6. Declare a class to be static.
+    static class Trucks
+    {
+        static void Foreign()
+        {
+
+        }
+
+    }
+}
---
+++ ClassAndMethod_5/ClassAndMethod_5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethod_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. In the Main() method, instantiate that class.
+            Car myCar = new Car();
+
+            //3.Have the user enter a number. Call the method on that number.
+            //Display the output to the screen. It should be the entered number, divided by two.
+            Console.WriteLine("Please enter an integer and I will divide it by two");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            myCar.Model(num1);
+
+            Console.WriteLine();
+            double num3 = 3.14; 
+            Console.WriteLine("3.14 divided by two is " + myCar.Model(num3));
+            Console.ReadLine();
+        }
+    }   
+}
