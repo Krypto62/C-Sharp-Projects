22603c97a8605772da1eadf0811c91c32863bafb
Scott <scf2005@gmail.com>
2018-11-03 15:01:05 -0700

    Initial Commit

---
+++ ClassAndSuperclass/ClassAndSuperclass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndSuperclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //4. Inside of the Main method, instantiate and initialize an Employee
+            // object with a first name of "Sample" and a last name of "Student".
+
+            Employee Person = new Employee();
+            Person.Firstname = "Sample";
+            Person.Lastname = "Student";
+
+            //5. Call the superclass method SayName() on the Employee object.
+            Person.SayName();
+        }
+    }
+}
