ae45e270b30a12473f482eb06832fd8e3500280c
Scott <scf2005@gmail.com>
2018-11-11 12:53:25 -0800

    Instantiate string and int lists

--- Generics/Generics/Employee.cs
+++ Generics/Generics/Employee.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Generics
 {
@@ -12,10 +9,7 @@ namespace Generics
         //2. Add a property to the Employee class called "things" and have its 
         //data type be a generic list matching the generic type of the class.
         public List<T> Things { get; set; }
-
-
     }
-
 }
 
 
--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Generics
 {
@@ -13,41 +10,31 @@ namespace Generics
             //3. Instantiate an Employee object with type "string" as its generic
             //parameter. Assign a list of strings as the property value of Things.
             Employee<string> object1 = new Employee<string>();
+            object1.Things = new List<string>();
             object1.Things.Add("Pen");
             object1.Things.Add("Pencil");
             object1.Things.Add("Stapler");
-            //foreach (string in object1.Things)
-            //{
-            //    Console.WriteLine(object1.Things);
-            //}   
-            Console.ReadLine();
+            foreach (string word in object1.Things)
+            {
+                Console.WriteLine(word);
+            }
+            Console.WriteLine();
 
             //4. Instantiate an Employee object with type "int" as its generic parameter.
             //Assign a list of integers as the property value of Things.
             Employee<int> object2 = new Employee<int>();
+            object2.Things = new List<int>();
             object2.Things.Add(7);
             object2.Things.Add(9);
             object2.Things.Add(11);
 
             //5. Create a loop that prints all of the Things to the Console.
+            foreach (int num in object2.Things)
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadLine();
 
-
-            //for (int i = 0; i < object1.Things.Count; i++)
-            //{
-            //    //print another list items.
-            //    for (int j = 0; j < object2.Things.Count; j++)
-            //    {
-            //        Console.WriteLine("/" + object1.Things[i] + "/" + object2.Things[j]);
-            //    }
-
-            //    Console.WriteLine("{0}\t", object2.Things);
-
-            //}
-            //foreach (string thing in object1.Things)
-            //{
-            //    Console.WriteLine(T thing);
-            //}
-            //Console.ReadLine();
         }
     }
 }
