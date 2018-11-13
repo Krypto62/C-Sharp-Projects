209ee56d8d0f6edc985fcbe053df0e282ecbea8a
Scott <scf2005@gmail.com>
2018-11-12 16:55:12 -0800

    Used { } for adding objects to lists

--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -10,10 +10,8 @@ namespace Generics
             //3. Instantiate an Employee object with type "string" as its generic
             //parameter. Assign a list of strings as the property value of Things.
             Employee<string> object1 = new Employee<string>();
-            object1.Things = new List<string>();
-            object1.Things.Add("Pen");
-            object1.Things.Add("Pencil");
-            object1.Things.Add("Stapler");
+            object1.Things = new List<string>() { "Pen", "Pencil", "Stapler"};
+
             foreach (string word in object1.Things)
             {
                 Console.WriteLine(word);
@@ -23,10 +21,7 @@ namespace Generics
             //4. Instantiate an Employee object with type "int" as its generic parameter.
             //Assign a list of integers as the property value of Things.
             Employee<int> object2 = new Employee<int>();
-            object2.Things = new List<int>();
-            object2.Things.Add(7);
-            object2.Things.Add(9);
-            object2.Things.Add(11);
+            object2.Things = new List<int>() { 7, 9, 11};
 
             //5. Create a loop that prints all of the Things to the Console.
             foreach (int num in object2.Things)
