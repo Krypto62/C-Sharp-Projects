44ecb949654672f7c2d066e4a2e5859d1a63d004
Scott <scf2005@gmail.com>
2018-11-13 16:35:16 -0800

    Initial Commit

---
+++ Structs/Structs/Number.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Structs
+{
+    //1. Create a struct called Number and give it the property "Amount"
+    //and have it be of data type decimal.
+    struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Structs/Structs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. In the Main() method, create an object of data type Number
+            //and assign an amount to it.
+
+            Number num1 = new Number();
+            num1.Amount = 89;
+
+            //3. Print this amount to the console.
+            Console.WriteLine(num1.Amount);
+            Console.ReadLine();
+        }
+    }
+}
