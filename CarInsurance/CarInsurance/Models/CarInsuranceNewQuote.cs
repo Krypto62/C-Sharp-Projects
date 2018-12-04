2d2204853eab573f4ba8044cedba4a306cb4731a
Scott <scf2005@gmail.com>
2018-12-03 17:05:47 -0800

    Cleaned up extra lines and spacing

--- CarInsurance/CarInsurance/Models/CarInsuranceNewQuote.cs
+++ CarInsurance/CarInsurance/Models/CarInsuranceNewQuote.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
+﻿
 namespace CarInsurance.Models
 {
     public class CarInsuranceNewQuote //Get data out of SQL
